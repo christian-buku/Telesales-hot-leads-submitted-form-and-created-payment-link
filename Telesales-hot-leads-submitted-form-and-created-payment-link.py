96e5779a300e2d9788861cb54ad13684541c7f03
christian-buku <168441078+christian-buku@users.noreply.github.com>
2025-08-08 12:35:55 +0700

    Update Telesales-hot-leads-submitted-form-and-created-payment-link.py

--- Telesales-hot-leads-submitted-form-and-created-payment-link.py
+++ Telesales-hot-leads-submitted-form-and-created-payment-link.py
@@ -170,7 +170,8 @@ with st.sidebar:
     st.header("Media (optional)")
     # image_url = st.text_input("Image URL (optional)")
     image_url = "https://raw.githubusercontent.com/christian-buku/Telesales-hot-leads-submitted-form-and-created-payment-link/refs/heads/main/banner.jpg"
-    image_filename = st.text_input("Image filename", value="banner.jpg")
+    # image_filename = st.text_input("Image filename", value="banner.jpg")
+    image_filename = "banner.jpg"
 
     st.divider()
     st.header("Source")
